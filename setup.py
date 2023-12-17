ceb42f09ffde8e6cc12090d55007032da6c06b1a
Renmin Pretell <125953348+RPretellD@users.noreply.github.com>
2023-12-17 16:03:56 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r", encoding = 'utf-8') as fp:
 
 setup(
 	name="gmms",
-	version="0.1.0",
+	version="0.1.1",
 	description="Ground motion models and supporting tools.",
 	author="A. Renmin Pretell Ductram",
 	author_email='rpretell@unr.edu',
